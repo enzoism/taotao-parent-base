172265ddad9fda8e38d03d45556f33c15160bd46
enzoism <2550753060@qq.com>
2019-05-14 23:22:16 +0800

    002--添加dao和mapper代码，然后进行json功能测试

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,10 @@
+package com.taotao.service;
+
+import com.taotao.pojo.TbItem;
+
+/**
+ * Created by zhifeng on 2019/5/14.
+ */
+public interface ItemService {
+    TbItem getItemById(Long itemId);
+}
---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -0,0 +1,30 @@
+package com.taotao.controller;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+import com.taotao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	private TbItem getItemById(@PathVariable Long itemId) {
+		TbItem item = itemService.getItemById(itemId);
+//		TbItem item = new TbItem();
+//		item.setNum(Integer.valueOf(11111111));
+		return item;
+	}
+}
